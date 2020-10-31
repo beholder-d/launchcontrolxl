20ea2ba8c37a0befde26cb685a690325c9af0bee
beholder-d <beholder.dash.d@gmail.com>
2020-10-31 13:46:01 -0700

    Adding example with incremental cc 10-65 for midi channels 0-7 and writing resulting sysexes into ./template-example

---
+++ src/Example/inc-cc-ch1-ch8.ts
@@ -0,0 +1,36 @@
+/**
+ * This file is generating templates with all controllers assigned to 
+ * incremental cc starting from 10-65 midi channels 0-7
+ * see ./example/incremental
+ * node ./build/src/Example/inc-cc-ch1-ch8.js
+ */
+import * as fs from 'fs';
+import LaunchControlXl from '../LaunchControlXl';
+import { ControllerType } from '../Common/ControllerType';
+import { ledColorList } from '../Common/LedColor';
+import { MessageType } from '../Common/MessageType';
+import { midiChannelRange } from '../Common/MidiChannel';
+
+{
+    const path = `./template-example`
+    if (!fs.existsSync(path)) {
+        fs.mkdirSync(path);
+    }
+
+    const lcxl = new LaunchControlXl();
+    for(let channel=0; channel<8; channel++) {
+        lcxl.controller.forEach((ctrl, i) => {
+            ctrl.messageType = MessageType.CC;
+            ctrl.cc = 10 + i;
+            ctrl.midiChannel = midiChannelRange(channel);
+            if(ctrl.controllerType === ControllerType.POTENTIOMETER) {
+                ctrl.ledColor = ledColorList[((i % LaunchControlXl.UNITS_PER_ROW) + 1) % ledColorList.length];
+                ctrl.ledMidiChannel = midiChannelRange(channel);
+            }
+        });
+
+        const sysex = lcxl.toSysex();
+        const sysexFilename = `${path}/inc-cc-ch${channel+1}.syx`;
+        fs.writeFileSync(sysexFilename, sysex);
+    }
+}
